346430a1fd236f13ff7324c59620a7b6bbd2cf93
CarmenRodriguezCardenal22 <carmen.rodriguezcardenal@iesvalleinclan.es>
2024-11-18 11:11:30 +0100

    A

--- Moviles/Tema6/Act6_4/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Moviles/Tema6/Act6_4/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +14,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ListView lista;
+    private TextView texto;
+    private RadioButton radio_button_pulsado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +34,58 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        lista=(ListView) findViewById(R.id.lista);
+        texto=(TextView) findViewById(R.id.texto);
+        ArrayList<Encapsulador> datos=new ArrayList<Encapsulador>();
+        datos.add(new Encapsulador(R.drawable.ima1, "DONUTS", "El 15 de septiembre de 2009, fue lanzado el SDK de Android 1.6 Donut, basado en el núcleo Linux 2.6.29. En la actualización se incluyen numerosas características nuevas.", true));
+        datos.add(new Encapsulador(R.drawable.ima2, "FROYO", "El 20 de mayo de 2010, El SDK de Android 2.2 Froyo (Yogur helado) fue lanzado, basado en el núcleo Linux 2.6.32.", false));
+        datos.add(new Encapsulador(R.drawable.ima3, "GINGERBREAD", "El 6 de diciembre de 2010, el SDK de Android 2.3 Gingerbread (Pan de Jengibre) fue lanzado, basado en el núcleo Linux 2.6.35.", false));
+        datos.add(new Encapsulador(R.drawable.ima4, "HONEYCOMB", "El 22 de febrero de 2011, sale el SDK de Android 3.0 Honeycomb (Panal de Miel). Fue la primera actualización exclusiva para TV y tableta, lo que quiere decir que sólo es apta para TV y tabletas y no para teléfonos Android.", false));
+        datos.add(new Encapsulador(R.drawable.ima5, "ICE CREAM", "El SDK para Android 4.0.0 Ice Cream Sandwich (Sándwich de Helado), basado en el núcleo de Linux 3.0.1, fue lanzado públicamente el 12 de octubre de 2011.", false));
+        datos.add(new Encapsulador(R.drawable.ima6, "JELLY BEAN", "Google anunció Android 4.1 Jelly Bean (Gomita Confitada o Gominola) en la conferencia del 30 de junio de 2012. Basado en el núcleo de linux 3.0.31, Bean fue una actualización incremental con el enfoque primario de mejorar la funcionalidad y el rendimiento de la interfaz de usuario.", false));
+        datos.add(new Encapsulador(R.drawable.ima7, "KITKAT", "Su nombre se debe a la chocolatina KitKat, de la empresa internacional Nestlé. Posibilidad de impresión mediante WIFI. WebViews basadas en el motor de Chromium.", false));
+        datos.add(new Encapsulador(R.drawable.ima8, "LOLLIPOP", "Incluye Material Design, un diseño intrépido, colorido, y sensible interfaz de usuario para las experiencias coherentes e intuitivos en todos los dispositivos. Movimiento de respuesta natural, iluminación y sombras realistas y familiares elementos visuales hacen que sea más fácil de navegar su dispositivo.", false));
+        lista.setAdapter(new Adaptador(this, R.layout.activity_adaptador, datos){
+            @Override
+            public void onEntrada(Object entrada, View view){
+                TextView texto_superior_entrada = (TextView) view.findViewById(R.id.texto_titulo);
+                TextView texto_inferior_entrada = (TextView) view.findViewById(R.id.texto_datos);
+                ImageView imagen_entrada = (ImageView) view.findViewById(R.id.imagen);
+                RadioButton miRadio = (RadioButton)  view.findViewById(R.id.boton);
+                miRadio.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(radio_button_pulsado != null) radio_button_pulsado.setChecked(false);
+                        radio_button_pulsado = (RadioButton) view;
+                        texto.setText("MARCADA UNA OPCIÓN");
+                    }
+                });
+            }
+        });
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> pariente, View view, int pos, long id){
+                Encapsulador elegido = (Encapsulador) pariente.getItemAtPosition(pos);
+                CharSequence textoElegido = "Seleccionado: " + elegido.getContenido();
+                texto.setText(elegido.getContenido());
+            }
+
+        });
+    }
+
+    public class Encapsulador{
+        private int imagen;
+        private String titulo, texto;
+        boolean dato1;
+        public Encapsulador(int idImagen, String textoTitulo, String textoContenido, boolean favorito){
+            this.imagen=idImagen;
+            this.titulo=textoTitulo;
+            this.texto=textoContenido;
+            this.dato1=favorito;
+        }
+        public String getTitulo(){return  titulo;}
+        public String getContenido(){return texto;}
+        public int getImagen(){return imagen;}
+        public boolean get_checkBox1(){return dato1;}
     }
 }
