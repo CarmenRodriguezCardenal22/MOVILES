9fbfda1937f25c4e91a97948cd65394961ca404f
CarmenRodriguezCardenal22 <carmen.rodriguezcardenal@iesvalleinclan.es>
2024-12-03 09:12:39 +0100

    A

--- Moviles/AppDeGestion/app/src/main/java/com/example/myapplication/Insertar.java
+++ Moviles/AppDeGestion/app/src/main/java/com/example/myapplication/Insertar.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
@@ -21,9 +24,25 @@ public class Insertar extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-    }
-    public void insertar(){
         EditText nombre = findViewById(R.id.Nombre); // Campo usuario
         EditText descripcion = findViewById(R.id.Descripcion);
+        Button boton = findViewById(R.id.boton);
+
+        Intent intent=new Intent();
+        if(intent!=null){
+            nombre.setText("nombre");
+            descripcion.setText("descripcion");
+        }
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insertar();
+            }
+        });
+    }
+    public void insertar(){
+        String nombre = nombre.getText().toString();
+        String descripcion = descripcion.getText().toString();
+
     }
 }
