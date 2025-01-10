85c1e72546bdf8f0bf822e6e31ee8d747473f77d
CarmenRodriguezCardenal22 <carmen.rodriguezcardenal@iesvalleinclan.es>
2025-01-10 11:13:15 +0100

    A

---
+++ Moviles/Tema9/Act9_1/app/src/main/java/com/example/myapplication/Fragmento1.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+public class Fragmento1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_fragmento1, container, false);
+    }
+}
---
+++ Moviles/Tema9/Act9_2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.contenedor), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        FragmentManager FM = getSupportFragmentManager();
+        FragmentTransaction FT = FM.beginTransaction();
+        Fragmento1 fragment1 = new Fragmento1();
+        FT.add(R.id.contenedor, fragment1);
+        FT.commit();
+
+        Fragmento2 fragment2 = new Fragmento2();
+        FT.replace(R.id.contenedor, fragment2);
+        FT.commit();
+    }
+
+
+}
